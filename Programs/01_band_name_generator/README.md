5ab14c4b9396c2b962c7ced83ca5737cced24e08
Diksha Garg <70652158+msDikshaGarg@users.noreply.github.com>
2024-01-09 22:37:43 -0500

    Adding comments and readme to the band generator

--- Programs/01_band_name_generator/README.md
+++ Programs/01_band_name_generator/README.md
@@ -1,2 +1,8 @@
 # Random Band Name Generator
 
+![Band kids](..GIFs/giphy_band.gif)
+
+
+In this program, we are supposed to take user input and generate a band name using a place name and type of animal. 
+
+The programs takes two user inputs for the name of the band and prints out the name of the band by concatinating the names. The play again loop asks the user if they want to generate a new name or if they are satisfied and want to exit.
--- Programs/01_band_name_generator/band_name_generator.py
+++ Programs/01_band_name_generator/band_name_generator.py
@@ -1,4 +1,4 @@
-# Importing os to clear screen and art from artwork.py
+# Importing needed libraries and files
 import os
 from artwork import art
 
@@ -6,20 +6,22 @@ from artwork import art
 play_again = True
 
 while play_again != False:
+    # Printing greeting
     print(art)
-
-    # Asking city name 
     print("Let's generate a cool band name for you.\n")
+
+    # Asking for a city name 
     city = input("Name a random place.\n")
 
-    # Asking pet name 
+    # Asking for an animal name 
     animal = input("Name a random animal in plural.\n")
 
     # Printing band name 
     print("Congratulations, your band name is ... " + city + " " + animal + "!")
 
-    # Play again loop: if the player wants to exit the play_again flag sets to false and we exit the main loop
-    again = input("Type 'Yes' to play again or 'No' to exit.\n")
+    # Play again loop: if the player wants to exit 
+    # the play_again flag sets to false and we exit the main loop
+    again = input("Type 'Yes' if you want to generate a new name or 'No' to exit.\n")
     while again not in ['yes', 'no']:
         again = input("Please enter a valid input.\n").lower()
     if again == 'no':
